662c41ea9f8f9aceb286f10ec727cbedba709f4c
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-04-08 02:42:24 +0530

    change eslint config according to readme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,7 @@
-const { EndOfLineState } = require("typescript");
-
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    ecmaVersion: "latest",
     project: "tsconfig.json",
     tsconfigRootDir: __dirname,
     sourceType: "module",
